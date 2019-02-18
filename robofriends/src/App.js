92390a006ffc2d2f1637aecf64c79c016f64f567
Kristoffer Stobbe <krisstobbe@yahoo.com>
2019-02-18 16:05:20 -0600

    Added Scrolling Div Element and Updated Font

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -2,19 +2,31 @@ import React from 'react';
 import CardList from './CardList';
 import {robots} from './robots';
 import SearchBox from './SearchBox.js';
+import './App.css';
+import Scroll from './Scroll.js';
 
 
 class App extends React.Component {
 	constructor() {
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
 	}
 
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response=> {
+			return response.json();
+		})
+		.then(users => {
+			this.setState({ robots: users})
+		});
+	}
+
 	onSearchChange = (event) => {
-		this.setState({searchfield: event.target.value});
+		  this.setState({searchfield: event.target.value});
 	
 	}
 
@@ -22,14 +34,20 @@ class App extends React.Component {
 	const filteredRobots = this.state.robots.filter(robots => {
 			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 	})
-	return (
+	if (this.state.robots.length === 0) {
+		return <h1 className = 'Load'>Loading</h1>
+	} else {
+		return (
 		<div className = 'tc'>
-			<h1> RoboFriends</h1>
+			<h1 className = 'f1'> RoboFriends</h1>
 			<SearchBox searchChange={this.onSearchChange}/>
-			<CardList robots={filteredRobots}/>
+			<Scroll>
+				<CardList robots={filteredRobots}/>
+			</Scroll>
 		</div>
 		);
 	}
+  }	
 }
 
 export default App;
