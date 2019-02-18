c08d593bf9a5cd6760e823e0673feee226c838fb
Kristoffer Stobbe <krisstobbe@yahoo.com>
2019-02-18 15:16:36 -0600

    Updating App
    Created Multiple Java Script Files Including the Cards, Robots, CardList, and the Search Function

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CardList from './CardList';
+import {robots} from './robots';
+import SearchBox from './SearchBox.js';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+class App extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			robots: robots,
+			searchfield: ''
+		}
+	}
+
+	onSearchChange = (event) => {
+		this.setState({searchfield: event.target.value});
+	
+	}
+
+	render() {
+	const filteredRobots = this.state.robots.filter(robots => {
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+	})
+	return (
+		<div className = 'tc'>
+			<h1> RoboFriends</h1>
+			<SearchBox searchChange={this.onSearchChange}/>
+			<CardList robots={filteredRobots}/>
+		</div>
+		);
+	}
 }
 
 export default App;
